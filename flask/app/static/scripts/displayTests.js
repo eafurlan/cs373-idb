6ef794b58506b7eeeb92304fe7ca6a87509a975f
eafurlan <furlaneaf@gmail.com>
2016-03-31 13:32:08 -0500

    about us page pulls from api now to get tests

--- flask/app/static/scripts/displayTests.js
+++ flask/app/static/scripts/displayTests.js
@@ -2,12 +2,17 @@
 jQuery(document).ready(function($) {
     $("button").click(function(){    
     	var $btn = $(this).button('Running...')
-	    output = "hello" + " world "; //call python function here
-	    $("#unitTestAlert").text(output);
+
+    	$.get("/test", function(data){
+    		$("#unitTestAlert").text( data.test_text );
+    	});
+
+	    // output = "hello" + " world "; //call python function here
+	    // $("#unitTestAlert").text(output);
 
 	    $("#unitTestAlert").removeClass("alert-danger");
 	    $("#unitTestAlert").removeClass("alert-success");
-	  	if(output.includes("Failed")){
+	  	if($("#unitTestAlert").text().includes("Failed")){
 	  		$("#unitTestAlert").addClass("alert-danger");
 	  	}
 	  	else{
