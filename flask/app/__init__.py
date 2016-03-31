e76879cccb9621d6a66067abf2e3ee13f3dd2ec2
oliver <olive.croomes@utexas.edu>
2016-03-31 13:16:43 -0500

    Created a GET endpoint for obtaining live test results.

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,4 +1,16 @@
 from flask import Flask
 
+from flask_restful import Api, Resource
+
 app = Flask(__name__)
 from app import views
+
+api = Api(app)
+
+class TestResource(Resource):
+    def get(self):
+        return {'test_text':'HELLO LIZ! THE API WORKS'}
+
+api.add_resource(TestResource,'/test')
+
+    
