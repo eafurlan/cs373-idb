ac2523721d863356f5be93dcb32aacf0fd8b1ceb
eafurlan <furlaneaf@gmail.com>
2016-04-05 14:10:07 -0500

    tables load from our api now

--- flask/app/static/scripts/sortBills.js
+++ flask/app/static/scripts/sortBills.js
@@ -1,36 +1,16 @@
 angular.module('sortApp', [])
 
-.controller('mainController', function($scope) {
+.controller('mainController', function($scope, $http) {
   $scope.sortType     = 'name'; // set the default sort type
   $scope.sortReverse  = false;  // set the default sort order
   
-  // create the list of bills - elements need not be ordered, but keys can't be in quotes.
-  $scope.bills = [
-    {
-      "name": "Pay Stub Disclosure Act",
-      "bill_type": "senate_bill",
-      "current_status": "referred",
-      "sponsor": 412378,
-      "date": "2016-03-03",
-      "id": 343921
-    },
-    {
-      "name": "Comprehensive Addiction and Recovery Act of 2016",
-      "bill_type": "senate_bill",
-      "current_status": "pass_over_senate",
-      "sponsor": 412247,
-      "date": "2015-02-12",
-      "id": 336967
-    },
-    {
-      "name": "Behavioral Health Coverage Transparency Act of 2016",
-      "bill_type": "senate_bill",
-      "current_status": "referred",
-      "sponsor": 412542,
-      "date": "2016-03-07",
-      "id": 343960
-    }
-];
+  // Load bills from our API
+	$http({
+		method: 'GET',
+		url: '/api/bills'
+	}).success(function (result){
+		$scope.bills = result;
+	});
   
 });
 
