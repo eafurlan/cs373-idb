862531609b8091486592e7a9966d58ffc83dba90
Lan Nguyen <lnguyen@cs.utexas.edu>
2016-04-07 13:40:28 -0500

    tests now run on button click

--- flask/app/static/scripts/displayTests.js
+++ flask/app/static/scripts/displayTests.js
@@ -1,26 +1,22 @@
 
 jQuery(document).ready(function($) {
-    $("#unitTestAlert").hide();
     $("button").click(function(){    
-    	var $btn = $(this).button('Running...');
+    	//var $btn = $(this).button('Running...');
+    	$('#unitTestButton').val() = 'Running ... '
+    	$("#unitTestAlert").removeClass("alert-success");
+    	$.get("/test", function(data){
+   			$("#unitTestAlert").html( data.test_text );
+   			$("#unitTestAlert").removeClass("alert-danger");
+	    	$("#unitTestAlert").removeClass("alert-success");
+		  	if($("#unitTestAlert").text().includes("Failed")){
+		  		$("#unitTestAlert").addClass("alert-danger");
+		  	}
+		  	else{
+		  		$("#unitTestAlert").addClass("alert-success");
+		  	}
+    });
 
-    	
-    	$("#unitTestAlert").slideToggle( "slow",function(){
-
-    	});
-    	
-
-	    // output = "hello" + " world "; //call python function here
-	    // $("#unitTestAlert").text(output);
-
-	    $("#unitTestAlert").removeClass("alert-danger");
-	    $("#unitTestAlert").removeClass("alert-success");
-	  	if($("#unitTestAlert").text().includes("Failed")){
-	  		$("#unitTestAlert").addClass("alert-danger");
-	  	}
-	  	else{
-	  		$("#unitTestAlert").addClass("alert-success");
-	  	}
+	   
 
     	$btn.button('reset')
   });
