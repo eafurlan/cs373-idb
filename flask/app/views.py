8d44eba03b45c5a874f7cff52fa0cd149ed00fc5
Chareth Cutestory <nuugit@gmail.com>
2016-04-07 12:09:59 -0500

    show name instead of number for sponsor on bills page; show all bills sponsored
    need to figure out different way of displaying all bills sponsored

--- flask/app/views.py
+++ flask/app/views.py
@@ -79,22 +79,36 @@ def render_person(person_id):
 	# print(type(query_assoc))
 
 	# this would find the id of first bill that this leg sponsord
-	assoc_query = session.query(Legislator, SponsorBillAssociation).join(SponsorBillAssociation).filter(Legislator.id == person_id)
+	# assoc_query = session.query(Legislator, SponsorBillAssociation).join(SponsorBillAssociation).filter(Legislator.id == person_id)
 	
 	# the sponsor bill association is the second tuple.
-	assoc_obj = assoc_query.first()[1]
-	assoc_obj_dict = assoc_obj.__dict__
+	# assoc_obj = assoc_query.first()[1]
+	# assoc_obj_dict = assoc_obj.__dict__
 	# assoc_dict = assoc_result.__dict__
 	# print(type(leg_obj))
 	# print(type(assoc_obj))
 
-	return render_template('legislators_template.html', person = leg_dict, sponsored_bill_association = assoc_obj_dict)
+
+	spon_query = session.query(SponsorBillAssociation).filter_by(leg_id=person_id).filter_by(type_of_sponsorship='sponsor')
+	spon_dict_list = [x.__dict__ for x in spon_query]
+	return render_template('legislators_template.html', person = leg_dict, sponsored_bill_association = spon_dict_list)
+	# return render_template('legislators_template.html', person = leg_dict, sponsored_bill_association = assoc_obj_dict)
+
 
 @app.route('/bills/<bill_id>')
 def render_bill(bill_id):
 	bill_obj = session.query(Bill).filter_by(id=bill_id).first()
 	bill_dict = bill_obj.__dict__
-	print(str(bill_dict))
+
+	spon_query = session.query(Legislator, SponsorBillAssociation).join(SponsorBillAssociation).filter(SponsorBillAssociation.bill_id==bill_id).filter(SponsorBillAssociation.type_of_sponsorship=='sponsor').first()
+	spon_dict = spon_query[1].__dict__
+	spon_query_name_dict = spon_query[0].__dict__
+	name = spon_query_name_dict['firstname'] + " " + spon_query_name_dict['lastname']
+	bill_dict['sponsor'] = {'id': spon_dict['leg_id'] , 'name':name}
+
+	# TODO: pull list of cosponsors
+	cospon_query = session.query(Legislator, SponsorBillAssociation).join(SponsorBillAssociation).filter(SponsorBillAssociation.bill_id==bill_id).filter(SponsorBillAssociation.type_of_sponsorship=='cosponsor').first()
+
 	return render_template('bills_template.html', bill = bill_dict)
 
 @app.errorhandler(404)
