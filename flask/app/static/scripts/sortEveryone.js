1fd149b1acdce25bdb714cf2175230878326232b
oliver <olive.croomes@utexas.edu>
2016-04-20 03:11:03 -0500

    UGHHHHHHHHHHHH, on the plus side I learned a lot about javascript. ON the downside, I had to touch javscript. Search works now at least for bills. I'll have to clean it up to be like case insensitive and all that, but it's basically done.

--- flask/app/static/scripts/sortEveryone.js
+++ flask/app/static/scripts/sortEveryone.js
@@ -1,113 +1,104 @@
 var sortApp = angular.module('sortApp', ['angularUtils.directives.dirPagination']);
 
 sortApp.config(['$locationProvider', function($locationProvider) {
-     $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
 }]);
 
 
-sortApp.filter('bill_OR_search',function(){
+sortApp.filter('bill_OR_search', function() {
 
-	return function(data,search){
+    return function(data, search) {
 
 
-		filtered_data = []
-		if(!search){
-			return filtered_data
-		}
-		
-		all_terms = search.split(' ')
+            filtered_data = [];
+            if (!search) {
+                return filtered_data;
+            }
 
-		for ( var a = 0; a < all_terms.length; ++a){
-			term = all_terms[a].toLowerCase();
-			for ( var b = 0; b < data.length; ++b){
-				datum = data[b];
-				//checks each property for the value.
-				if(datum.name.toLowerCase().includes(term) || datum.current_status.toLowerCase().includes(term)){
-					//this data is cool
-					filtered_data.push(datum);
-				}
+            all_terms = search.split(' ')
 
-			}
+            data.forEach(function(datum) {
+                push_flag = false;
+                all_terms.forEach(function(term) {
+                    if (datum.name.includes(term)) {
+                        push_flag = true;
+                    }
+                })
+                if (push_flag) {
+                    filtered_data.push(datum);
+                }
+            });
 
-		}
-		return filtered_data;
-	}//end inner func
+            return filtered_data;
+        } //end inner func
 
 });
 
 
-sortApp.filter('bill_AND_search',function(){
+sortApp.filter('bill_AND_search', function() {
 
-	return function(data,search){
+    return function(data, search) {
 
-		filtered_data = [];
-		if(!search){
-			return filtered_data;
-		}
-		
-		all_terms = search.split(' ')
 
-		for(var data_ind = 0; data_ind < data.length; ++data_ind){
-			datum = data[data_ind];
-			flag = true;
-			for(var term_ind = 0; term_ind < all_terms.length; ++term_ind){
-				console.log("reaches here");
-				term = all_terms[term_ind];
-				if(datum.name.toLowerCase().includes(term) || datum.current_status.toLowerCase().includes(term)){
-					console.log("It contains em!");
-					break;
-					//this data is cool
-				}
-				else{
-					flag = false;
-				}
+            filtered_data = [];
+            if (!search) {
+                return filtered_data;
+            }
 
-			}
-			if(flag)
-				filtered_data.push(datum);
-		}
+            all_terms = search.split(' ');
 
-		
-		return filtered_data;
-	}//end inner func
+            data.forEach(function(datum) {
+                push_flag = true;
+                all_terms.forEach(function(term) {
+                    if (!datum.name.includes(term)) {
+                        push_flag = false;
+                    }
+                })
+                if (push_flag) {
+                    filtered_data.push(datum);
+                }
+
+            });
+
+            return filtered_data;
+        } //end inner func
 
 });
 
 
 sortApp.controller('mainController', function($scope, $http, $location) {
- 	$scope.people = [];
-  	
-  // Load people from our API
-	$http({
-		method: 'GET',
-		url: 'http://0.0.0.0:8080/api/legislators'
-	}).success(function (result){
-		$scope.people = result;
-	});
+    $scope.people = [];
 
-	$http({
-		method: 'GET',
-		url: 'http://0.0.0.0:8080/api/bills'
-	}).success(function (result){
-		$scope.bills = result;
-	});
+    // Load people from our API
+    $http({
+        method: 'GET',
+        url: 'http://0.0.0.0:8080/api/legislators'
+    }).success(function(result) {
+        $scope.people = result;
+    });
 
-	
-	// $scope.makeLink = function () {
- //  		// $scope.selected = this.roll;
- //    //     path_to_person = $scope.selected.id + "";
- //    //     window.location.replace("legislators/"+path_to_person);
+    $http({
+        method: 'GET',
+        url: 'http://0.0.0.0:8080/api/bills'
+    }).success(function(result) {
+        $scope.bills = result;
+    });
 
- //    		$scope.selected = //SOMETHING
- //    		path_to_thing = $scope.selected.id + "";
- //    		if(/* The thing is a bill */){
- //    				window.location.replace("bills/"+path_to_person);
- //    		}
- //    		else if(/*The thing is a legislator*/){
- //    				window.location.replace("legislators/"+path_to_person);
- //    		}	
 
-	// };
-  
+    // $scope.makeLink = function () {
+    //  		// $scope.selected = this.roll;
+    //    //     path_to_person = $scope.selected.id + "";
+    //    //     window.location.replace("legislators/"+path_to_person);
+
+    //    		$scope.selected = //SOMETHING
+    //    		path_to_thing = $scope.selected.id + "";
+    //    		if(/* The thing is a bill */){
+    //    				window.location.replace("bills/"+path_to_person);
+    //    		}
+    //    		else if(/*The thing is a legislator*/){
+    //    				window.location.replace("legislators/"+path_to_person);
+    //    		}	
+
+    // };
+
 });
-
