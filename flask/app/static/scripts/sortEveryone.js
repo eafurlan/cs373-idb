160af02fc37fcfe4f7912fe1638106eba87fa6c9
oliver <olive.croomes@utexas.edu>
2016-04-21 07:56:11 -0500

    Created Prettier Display for Search, Consolidated 4 search views into one, added links on clicking.

--- flask/app/static/scripts/sortEveryone.js
+++ flask/app/static/scripts/sortEveryone.js
@@ -9,9 +9,9 @@ sortApp.filter('bill_OR_search', function() {
 
     return function(data, search) {
 
-
             filtered_data = [];
-            if (!search) {
+            
+            if (!search || search.length < 2) {
                 return filtered_data;
             }
             all_terms = search.toLowerCase().split(' ')
@@ -27,8 +27,9 @@ sortApp.filter('bill_OR_search', function() {
                         
                         push_flag = true;
                     }
-                })
+                });
                 if (push_flag) {
+                    datum._category = "Bill";
                     filtered_data.push(datum);
                 }
             });
@@ -44,7 +45,7 @@ sortApp.filter('bill_AND_search', function() {
     return function(data, search) {
 
             filtered_data = [];
-            if (!search) {
+            if (!search || search.length < 2) {
                 return filtered_data;
             }
 
@@ -63,6 +64,7 @@ sortApp.filter('bill_AND_search', function() {
                     }
                 });
                 if (push_flag) {
+                    datum._category = "Bill";
                     filtered_data.push(datum);
                 }
 
@@ -80,12 +82,11 @@ sortApp.filter('people_AND_search', function() {
     return function(data, search) {
 
             filtered_data = [];
-            if (!search) {
+            if (!search || search.length < 2) {
                 return filtered_data;
             }
 
             all_terms = search.split(' ');
-
             data.forEach(function(datum) {
                 push_flag = true;
                 var concatted = datum.birthday + datum.description 
@@ -101,8 +102,10 @@ sortApp.filter('people_AND_search', function() {
                     if (!(concatted.includes(term))) {
                         push_flag = false;
                     }
-                })
+                });
                 if (push_flag) {
+                                        datum._category = "Person";
+
                     filtered_data.push(datum);
                 }
 
@@ -114,12 +117,88 @@ sortApp.filter('people_AND_search', function() {
 });
 
 
+sortApp.filter('all_search',function(){
+
+
+    return function(data,search){
+        
+        filtered_data = [];
+
+        //Null and length check
+        if (!search || search.length < 2) {
+                return filtered_data;
+            }
+
+        all_terms = search.split(' ');
+
+        data.forEach(function(datum){
+            
+            var all_attribs = "";
+            var push_flag = false;
+            var and_flag = true;
+
+            if(datum._category === 'Bill'){
+                all_attribs = datum.name  
+                                + datum.bill_type 
+                                + datum.current_status 
+                                + datum.id 
+                                + datum.date;
+            }
+
+            if(datum._category === 'Person'){
+                all_attribs = datum.birthday 
+                                + datum.description 
+                                + datum.lastname 
+                                + datum.firstname 
+                                + datum.party
+                                + datum.start_date
+                                + datum.state
+                                + datum.title
+                                + datum.twitter
+                                + datum.youtube;
+            }
+
+            /*Goes through every search term.
+                every term present = AND
+                some terms present = OR
+                no terms present = no push
+            */
+            all_terms.forEach(function(term){
+                if(!all_attribs.includes(term)){
+                    //at least one attribute is not included
+                    and_flag = false;
+                }
+                else{
+                    //at least one attribute is included so pushing is okay
+                    push_flag = true;
+                }
+            });
+
+            if(and_flag){
+                datum._query_type = 'AND';
+            }
+            else{
+                datum._query_type = 'OR';
+            }
+
+            if(push_flag)
+            {
+                filtered_data.push(datum);
+            }
+
+        });
+
+        return filtered_data;
+    }
+
+})
+
 sortApp.filter('people_OR_search', function() {
 
     return function(data, search) {
 
             filtered_data = [];
-            if (!search) {
+            if (!search || search.length < 2) {
                 return filtered_data;
             }
 
@@ -140,8 +219,10 @@ sortApp.filter('people_OR_search', function() {
                     if (concatted.includes(term)) {
                         push_flag = true;
                     }
-                })
+                });
                 if (push_flag) {
+                    datum._category = "Person";
+
                     filtered_data.push(datum);
                 }
 
@@ -155,13 +236,16 @@ sortApp.filter('people_OR_search', function() {
 
 sortApp.controller('mainController', function($scope, $http, $location) {
     $scope.people = [];
-
-    // Load people from our API
+    $scope.everyone = [];
     $http({
         method: 'GET',
         url: 'http://0.0.0.0:8080/api/legislators'
     }).success(function(result) {
         $scope.people = result;
+        $scope.people.forEach(function(person){
+            person._category = 'Person';
+        });
+        $scope.everyone = $scope.everyone.concat($scope.people);
     });
 
     $http({
@@ -169,23 +253,26 @@ sortApp.controller('mainController', function($scope, $http, $location) {
         url: 'http://0.0.0.0:8080/api/bills'
     }).success(function(result) {
         $scope.bills = result;
+        $scope.bills.forEach(function(bill){
+            bill._category = 'Bill';
+        });
+        $scope.everyone = $scope.everyone.concat($scope.bills);
     });
 
 
-    // $scope.makeLink = function () {
-    //  		// $scope.selected = this.roll;
-    //    //     path_to_person = $scope.selected.id + "";
-    //    //     window.location.replace("legislators/"+path_to_person);
 
-    //    		$scope.selected = //SOMETHING
-    //    		path_to_thing = $scope.selected.id + "";
-    //    		if(/* The thing is a bill */){
-    //    				window.location.replace("bills/"+path_to_person);
-    //    		}
-    //    		else if(/*The thing is a legislator*/){
-    //    				window.location.replace("legislators/"+path_to_person);
-    //    		}	
 
-    // };
+    $scope.makeLink = function () {
+     		
+       		$scope.selected = this.roll;
+       		path_to_thing = $scope.selected.id + "";
+       		if($scope.selected._category === 'Bill'){
+       				window.location.replace("bills/"+path_to_thing);
+       		}
+       		else if($scope.selected._category === 'Person'){
+       				window.location.replace("legislators/"+path_to_thing);
+       		}	
+
+    };
 
 });
